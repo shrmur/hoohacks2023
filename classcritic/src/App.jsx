6e0a7d22055fcc887bf7ec5feccd89a3c1cf857d
shrmur <shrishpandi@gmail.com>
2023-03-25 13:55:22 -0400

    creating login form

--- classcritic/src/App.jsx
+++ classcritic/src/App.jsx
@@ -7,29 +7,23 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="box">
+      /* so here we'll add the nav bar */
+      <h1>Class Criqitue</h1>
+      
+      <h1>Login</h1>
+      <input type="text" placeholder='username'  />
+      <input type="password" placeholder='password' />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button type="button">Login</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <a href="www.google.com">Sign up Here!</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
 export default App
+
+
